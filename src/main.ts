db42638ef3ae28ba807ddb69ce575203e18ca186
unknown <sr.diegopinheiro@gmail.com>
2024-04-19 08:33:23 -0300

    Adding CORS setting

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,20 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuração para aceitar requisições de qualquer origem
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
+  });
+
+  // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('I7 Transport e Comercio - Fleet Admin')
     .setDescription('Aplicação para administração da frota.')
